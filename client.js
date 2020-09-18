444967257dcc3d19bc6620e328b12af4a70fc37c
Aleksej10 <mr.strale@gmail.com>
2020-09-18 21:49:01 +0200

    added google api test

--- client.js
+++ client.js
@@ -1,3 +1,54 @@
+const CLIENT_ID = '457555921399-86s8lincsm63un3ctbj3oo2n8uete63t.apps.googleusercontent.com';
+const API_KEY = 'AIzaSyCDEaAMRp2uCzRjtg_vr8hG2QdFeTLBKkw';
+var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
+const SCOPES = "https://www.googleapis.com/auth/calendar";
+
+function handleClientLoad() {
+    gapi.load('client:auth2', initClient);
+}
+
+function initClient() {
+    gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES,
+    }).then(function () {
+        // Listen for sign-in state changes.
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+        // Handle the initial sign-in state.
+        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        authorizeButton.onclick = handleAuthClick;
+        signoutButton.onclick = handleSignoutClick;
+    }, function(error) {
+          appendPre(JSON.stringify(error, null, 2));
+    });
+}
+
+function updateSigninStatus(isSignedIn) {
+    if (isSignedIn) {
+        authorizeButton.style.display = 'none';
+        signoutButton.style.display = 'block';
+        listUpcomingEvents();
+    } 
+    else {
+        authorizeButton.style.display = 'block';
+        signoutButton.style.display = 'none';
+    }
+}
+
+// Sign in the user upon button click.
+function handleAuthClick(event) {
+    gapi.auth2.getAuthInstance().signIn();
+}
+
+// Sign out the user upon button click.
+function handleSignoutClick(event) {
+    gapi.auth2.getAuthInstance().signOut();
+}
+
+
 document.onkeydown = function (e) {
     e = e || window.event;
     switch (e.which || e.keyCode) {
