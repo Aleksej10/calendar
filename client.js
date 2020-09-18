bd2a512fd7fec7b4e6e884498a0d3ff7df996f37
Aleksej10 <mr.strale@gmail.com>
2020-09-18 10:36:02 +0200

    added form validation

--- client.js
+++ client.js
@@ -1,4 +1,32 @@
+document.onkeydown = function (e) {
+    e = e || window.event;
+    switch (e.which || e.keyCode) {
+        case 13 : document.getElementById('submit-button').click();
+            break;
+    }
+}
+
 function submit(event){
-    console.log('hi');
+    const fields = [
+        document.getElementById('name'),
+        document.getElementById('phone'),
+        document.getElementById('email'),
+        document.getElementById('datetime'),
+    ];
+    for(var f of fields){
+        if(!f.validity.valid){
+            f.focus();
+            return;
+        }
+    }
+    const name = fields[0].value;
+    const phone = fields[1].value;
+    const email = fields[2].value;
+    const datetime = fields[3].value;
+    console.log("form is valid");
+    console.log(name);
+    console.log(phone);
+    console.log(email);
+    console.log(datetime);
 }
 
