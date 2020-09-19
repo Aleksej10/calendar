7058ccdd1790648d196dfddc6be321a5c5ef4df1
Aleksej10 <mr.strale@gmail.com>
2020-09-19 10:47:06 +0200

    list upcoming events, test

--- client.js
+++ client.js
@@ -32,9 +32,37 @@ function initClient() {
     });
 }
 
+function listUpcomingEvents() {
+gapi.client.calendar.events.list({
+  'calendarId': 'primary',
+  'timeMin': (new Date()).toISOString(),
+  'showDeleted': false,
+  'singleEvents': true,
+  'maxResults': 10,
+  'orderBy': 'startTime'
+}).then(function(response) {
+  var events = response.result.items;
+  appendPre('Upcoming events:');
+
+  if (events.length > 0) {
+    for (i = 0; i < events.length; i++) {
+      var event = events[i];
+      var when = event.start.dateTime;
+      if (!when) {
+        when = event.start.date;
+      }
+      appendPre(event.summary + ' (' + when + ')')
+    }
+  } else {
+    appendPre('No upcoming events found.');
+  }
+});
+}
+
 function updateSigninStatus(isSignedIn) {
     if (isSignedIn) {
         console.log("successfully signed in with google account!")
+        listUpcomingEvents();
         // authorizeButton.style.display = 'none';
         // signoutButton.style.display = 'block';
         // listUpcomingEvents();
